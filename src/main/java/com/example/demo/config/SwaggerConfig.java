ac024ac4e42406826e13086200efdc62bb2cbb8f
bala-amypo <bala@amypo.in>
2025-12-18 10:10:57 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,24 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
-demo/src/main/java/com/example/demo/configimport org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class swaggerConfig {
+public class SwaggerConfig { // Changed to capital 'S'
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info() // Added Info section
+                        .title("Drug Interaction Checker API")
+                        .version("1.0")
+                        .description("API for checking medication interactions"))
                 .servers(List.of(
                         new Server().url("https://9184.pro604cr.amypo.ai/")
                 ));
-        }
+    }
 }
