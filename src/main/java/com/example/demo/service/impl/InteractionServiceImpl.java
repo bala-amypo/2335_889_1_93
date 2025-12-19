1eeaf533ef6ef51c0a3d3c0406f00a3e95a0d135
bala-amypo <bala@amypo.in>
2025-12-19 05:02:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InteractionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InteractionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
+import com.example.demo.service.impl.ResourceNotFoundException;
 
-import com.example.demo.exception.ResourceNotFoundException;
+
+
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
 import com.example.demo.service.InteractionService;
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,32 +1,24 @@
 package com.example.demo.service.impl;
-import com.example.demo.service.impl.ResourceNotFoundException;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository,
-                           PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(User user) {
-
         if (user.getRole() == null) {
             user.setRole("USER");
         }
-
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
