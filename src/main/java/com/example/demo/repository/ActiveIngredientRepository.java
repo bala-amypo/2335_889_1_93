f7f2e431a482cb548b99edaf2f0462b60c07b91f
bala-amypo <bala@amypo.in>
2025-12-20 15:48:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActiveIngredientRepository.java
+++ src/main/java/com/example/demo/repository/ActiveIngredientRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ActiveIngredientRepository
         extends JpaRepository<ActiveIngredient, Long> {
+
+    boolean existsByName(String name);
 }
