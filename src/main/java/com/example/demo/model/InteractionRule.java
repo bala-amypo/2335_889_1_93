dc7b3061bf92d7a558ce6fbc1f03b23267607897
bala-amypo <bala@amypo.in>
2025-12-20 13:34:30 +0000

    User commit

--- src/main/java/com/example/demo/model/InteractionRule.java
+++ src/main/java/com/example/demo/model/InteractionRule.java
@@ -20,7 +20,7 @@ public class InteractionRule {
 
     @ManyToOne
     @JoinColumn(name = "ingredientB_id", nullable = false)
-    @JsonIgnore
+   
     private ActiveIngredient ingredientB;
 
     private String severity; // MINOR, MODERATE, MAJOR
