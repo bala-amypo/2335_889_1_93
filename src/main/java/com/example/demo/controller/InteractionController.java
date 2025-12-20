18e2ac30aa4b639f5f718ae92aa53e5d91d45e59
bala-amypo <bala@amypo.in>
2025-12-20 15:39:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/InteractionController.java
+++ src/main/java/com/example/demo/controller/InteractionController.java
@@ -4,10 +4,7 @@ import com.example.demo.model.ActiveIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface ActiveIngredientRepository extends JpaRepository<ActiveIngredient, Long> {
-    Optional<ActiveIngredient> findByName(String name);
-    boolean existsByName(String name);
+public interface ActiveIngredientRepository
+        extends JpaRepository<ActiveIngredient, Long> {
 }
