d0f24f70165b3a8044a3b840adb76acf132430cf
bala-amypo <bala@amypo.in>
2025-12-20 13:13:30 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public Map<String,String> handleAll(Exception ex){
-        Map<String,String> error = new HashMap<>();
-        error.put("error", ex.getMessage());
-        return error;
-    }
-}
+// @RestControllerAdvice
+// public class GlobalExceptionHandler {
+//     @ExceptionHandler(Exception.class)
+//     public Map<String,String> handleAll(Exception ex){
+//         Map<String,String> error = new HashMap<>();
+//         error.put("error", ex.getMessage());
+//         return error;
+//     }
+// }
