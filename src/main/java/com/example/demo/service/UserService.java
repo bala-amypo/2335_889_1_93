403e913aa912ccc8450320bc253dd59c3eef5812
bala-amypo <bala@amypo.in>
2025-12-26 16:52:19 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,15 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
-import java.util.List;
 
 public interface UserService {
 
-    User register(User user); // matches controller
+    User register(User user);
 
-    User findByEmail(String email); // unwrap Optional here
-
-    boolean existsByEmail(String email);
-
-    List<User> getAllUsers();
+    User findByEmail(String email);
 }
