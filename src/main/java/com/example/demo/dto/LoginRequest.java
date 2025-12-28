f219cf34f65e41a17f74398b4e93e64a1c344410
bala-amypo <bala@amypo.in>
2025-12-28 13:11:33 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,28 +1,28 @@
-package com.example.demo.dto;
+// package com.example.demo.dto;
 
-public class LoginRequest {
+// public class LoginRequest {
 
-    private String email;
-    private String password;
+//     private String email;
+//     private String password;
 
-    // No-arg constructor
-    public LoginRequest() {
-    }
+//     // No-arg constructor
+//     public LoginRequest() {
+//     }
 
-    // Getters & Setters
-    public String getEmail() {
-        return email;
-    }
+//     // Getters & Setters
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+// }
